0cd9cb2054fe00c4841c8a596e6f2e5e052af17d
Péter Sebők <sebok.peter97@gmail.com>
2018-10-08 10:56:00 +0200

    Added beer service interface.

--- ipcsmmd_webshop/ipcsmmd_webshop.Core/ApplicationService/IBeerService.cs
+++ ipcsmmd_webshop/ipcsmmd_webshop.Core/ApplicationService/IBeerService.cs
@@ -1,10 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ipcsmmd_webshop.Core.Entity;
 
 namespace ipcsmmd_webshop.Core.ApplicationService
 {
-    interface IBeerService
+    public interface IBeerService
     {
+        List<Beer> GetBeers();
+
+        IEnumerable<Beer> ReadBeers();
+
+        Beer AddBeer(Beer beer);
+
+        Beer RemoveBeer(int id);
+
+        Beer GetBeerByID(int id);
+
+        Beer UpdateBeer(Beer beer);
+
+        // TODO: Add filtering methonds
+        //List<Beer> GetFilteredBeers(BeerFilter filter);
+
+        List<Beer> GetBeersByType(BeerType type);
+
+        List<Beer> GetBeersByPrice(bool isAscending);
+        
     }
 }
