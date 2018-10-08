f729424877ab1030c40b93bed416b2ec2c208db5
Péter Sebők <sebok.peter97@gmail.com>
2018-10-08 12:16:05 +0200

    Work on Startup.cs

---
+++ ipcsmmd_webshop/ipcsmmd_webshop.Infrastructure.Data/WebShopContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace ipcsmmd_webshop.Infrastructure.Data
+{
+    class WebShopContext : DbContext
+    {
+    }
+}
