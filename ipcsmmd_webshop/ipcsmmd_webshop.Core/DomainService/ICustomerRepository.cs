8210b3e742f97615e566fc6529e34582460e7177
Dominik Nagy <32298986+NagyDominik@users.noreply.github.com>
2018-10-08 11:00:31 +0200

    ICustomerIRepository

--- ipcsmmd_webshop/ipcsmmd_webshop.Core/DomainService/ICustomerRepository.cs
+++ ipcsmmd_webshop/ipcsmmd_webshop.Core/DomainService/ICustomerRepository.cs
@@ -1,10 +1,26 @@
-﻿using System;
+﻿using ipcsmmd_webshop.Core.Entity;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ipcsmmd_webshop.Core.DomainService
 {
-    interface ICustomerRepository
+    public interface ICustomerRepository
     {
+        //Returns all Customers
+        IEnumerable<Customer> GetAll();
+        //Returns a filtered list of Customers
+        IEnumerable<Customer> GetFiltered(/*Filter filter*/);
+        //Returns a Customer by ID
+        IEnumerable<Customer> GetCustomerByID(int id);
+
+        //Saves a new Customer to the database 
+        Customer Save(Customer cust);
+
+        //Updates an existing Customer
+        Customer Update(int id, Customer cust);
+        
+        //Deletes a Customer by ID
+        Customer Remove(int id);
     }
 }
