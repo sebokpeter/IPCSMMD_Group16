9101a613890fe194e401346487c1036f06a63332
Péter Sebők <sebok.peter97@gmail.com>
2018-10-08 11:08:27 +0200

    Added order and customer service interfaces.

--- ipcsmmd_webshop/ipcsmmd_webshop.Core/ApplicationService/ICustomerService.cs
+++ ipcsmmd_webshop/ipcsmmd_webshop.Core/ApplicationService/ICustomerService.cs
@@ -1,10 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ipcsmmd_webshop.Core.Entity;
+
 
 namespace ipcsmmd_webshop.Core.ApplicationService
 {
     interface ICustomerService
     {
+        List<Customer> GetCustomers();
+
+        IEnumerable<Customer> ReadCustomers();
+
+        Customer GetCustomerByID(int id);
+
+        Customer AddCustomer(Customer customer);
+
+        Customer UpdateCustomer(Customer customer);
+
     }
 }
--- ipcsmmd_webshop/ipcsmmd_webshop.Core/ApplicationService/IOrderService.cs
+++ ipcsmmd_webshop/ipcsmmd_webshop.Core/ApplicationService/IOrderService.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ipcsmmd_webshop.Core.Entity;
+
 
 namespace ipcsmmd_webshop.Core.ApplicationService
 {
     interface IOrderService
     {
+        List<Order> GetOrders();
+
+        IEnumerable<Order> ReadOrders();
+
+        Order AddOrder(Order order);
+
+        Order RemoveOrder(int id);
+
+        Order GetOrderByID(int id);
+
+        Order UpdateOrder(Order order);
     }
 }
