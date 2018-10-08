2448e13cd5fdc2704b7deec2c30e709103ad784c
Dominik Nagy <32298986+NagyDominik@users.noreply.github.com>
2018-10-08 10:49:39 +0200

    Added entities

---
+++ ipcsmmd_webshop/ipcsmmd_webshop.Core.Entity/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ipcsmmd_webshop.Core.Entity
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public List<Order> Orders { get; set; }
+    }
+}
---
+++ ipcsmmd_webshop/ipcsmmd_webshop.Core.Entity/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ipcsmmd_webshop.Core.Entity
+{
+    public class Order
+    {
+        public int ID { get; set; }
+        public int CustomerID { get; set; }
+
+    }
+}
