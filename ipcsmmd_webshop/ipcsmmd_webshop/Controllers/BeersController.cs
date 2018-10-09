92771584a5c2a66762101c6d8a99f53f0ac1d8f4
Péter Sebők <sebok.peter97@gmail.com>
2018-10-09 13:10:42 +0200

    Updated BeersController

--- ipcsmmd_webshop/ipcsmmd_webshop/Controllers/BeersController.cs
+++ ipcsmmd_webshop/ipcsmmd_webshop/Controllers/BeersController.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using ipcsmmd_webshop.Core.ApplicationService;
 using ipcsmmd_webshop.Core.Entity;
 using Microsoft.AspNetCore.Http;
@@ -41,27 +39,115 @@ namespace ipcsmmd_webshop.Controllers
 
         // GET: api/Beers/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public ActionResult<Beer> Get(int id)
         {
-            return "value";
+            try
+            {
+                if (id < 1)
+                {
+                    return BadRequest("ID must be bigger than 0!");
+                }
+
+                Beer b = _service.GetBeerByID(id);
+                if (b == null)
+                {
+                    return NotFound($"We didn't find the beer with the ID of {id}");
+                }
+                return Ok(b);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         // POST: api/Beers
         [HttpPost]
-        public void Post([FromBody] string value)
+        public ActionResult<Beer> Post([FromBody] Beer value)
         {
+            try
+            {
+                value.ID = 0;
+                if (String.IsNullOrEmpty(value.Name))
+                {
+                    return BadRequest("Cannot add a beer without name!");
+                }
+                if (String.IsNullOrEmpty(value.Brand))
+                {
+                    return BadRequest("Cannot add a beer without brand!");
+                }
+
+                if (_service.AddBeer(value) != null)
+                {
+                    string s = String.Format($"Beer with the ID of {0} has been added!");
+                    return StatusCode(StatusCodes.Status201Created, s);
+                }
+                else
+                {
+                    return BadRequest("Could not add the beer!");
+                }
+            }
+            catch ( Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         // PUT: api/Beers/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public ActionResult<Beer> Put(int id, [FromBody] Beer value)
         {
+            try
+            {
+                if (id < 1)
+                {
+                    return BadRequest("Cannot update non-existing beer!");
+                }
+                if (value.ID != id)
+                {
+                    return BadRequest("Parameter mismatch: supplied ID and beer ID are not equal!");
+                }
+
+                if (_service.UpdateBeer(value) != null)
+                {
+                    return Ok($"Beer with the ID of {id} has been updated!");
+                }
+                else
+                {
+                    return BadRequest($"Could not update beer with the ID of {id}");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            
         }
 
         // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public ActionResult<Beer> Delete(int id)
         {
+            try
+            {
+                if (id < 1)
+                {
+                    return BadRequest("Cannot delete a non-existing beer!");
+                }
+
+                if (_service.RemoveBeer(id) != null)
+                {
+                    return Ok($"Beer with the ID of {id} has been deleted!");
+                }
+                else
+                {
+                    return BadRequest($"Could not delete beer with the ID of {id}");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
