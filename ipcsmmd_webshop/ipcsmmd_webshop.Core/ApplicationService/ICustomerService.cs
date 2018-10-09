8b5596badd8b3aa7e2152f125ab46d86b8118bfd
Dominik Nagy <32298986+NagyDominik@users.noreply.github.com>
2018-10-09 10:20:59 +0200

    AddCustomer + Test

--- ipcsmmd_webshop/ipcsmmd_webshop.Core/ApplicationService/ICustomerService.cs
+++ ipcsmmd_webshop/ipcsmmd_webshop.Core/ApplicationService/ICustomerService.cs
@@ -8,15 +8,16 @@ namespace ipcsmmd_webshop.Core.ApplicationService
 {
     public interface ICustomerService
     {
-        List<Customer> GetCustomers();
-
+        //Returns a list of Customers from the Infrastructure
         IEnumerable<Customer> ReadCustomers();
 
+        //Returns a Customer by the provided ID from the Infrastructure
         Customer GetCustomerByID(int id);
 
+        //Attempts to save a new Customer
         Customer AddCustomer(Customer customer);
 
+        //Attempts to update an existing Customer
         Customer UpdateCustomer(Customer customer);
-
     }
 }
---
+++ ipcsmmd_webshop/ipcsmmd_webshop.Core/ApplicationService/Impl/CustomerService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using ipcsmmd_webshop.Core.DomainService;
+using ipcsmmd_webshop.Core.Entity;
+
+namespace ipcsmmd_webshop.Core.ApplicationService.Impl
+{
+    public class CustomerService : ICustomerService
+    {
+        private ICustomerRepository _crepo;
+
+        public CustomerService(ICustomerRepository repository)
+        {
+            _crepo = repository;
+        }
+
+        public Customer AddCustomer(Customer customer)
+        {
+            if (customer.ID != 0)
+                throw new InvalidDataException("Cannot add customer with existing ID!");
+            if (customer.FirstName == null)
+                throw new InvalidDataException("Cannot add customer without first name!");
+            if (customer.LastName == null)
+                throw new InvalidDataException("Cannot add customer without last name!");
+            if (customer.Email == null)
+                throw new InvalidDataException("Cannot add customer without email address!");
+            if (customer.Address == null)
+                throw new InvalidDataException("Cannot add customer without address!");
+
+            return _crepo.Save(customer);
+        }
+
+        public Customer GetCustomerByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Customer> ReadCustomers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer UpdateCustomer(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
