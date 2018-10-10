2b57ab52c685b3ca238fc8efb871b1bdd60f03b9
Péter Sebők <sebok.peter97@gmail.com>
2018-10-10 11:05:43 +0200

    Update ipcsmmd_webshop/ipcsmmd_webshop.Infrastructure.Data/Repositories/CustomerRepository.cs

--- ipcsmmd_webshop/ipcsmmd_webshop.Infrastructure.Data/Repositories/CustomerRepository.cs
+++ ipcsmmd_webshop/ipcsmmd_webshop.Infrastructure.Data/Repositories/CustomerRepository.cs
@@ -41,10 +41,12 @@ namespace ipcsmmd_webshop.Infrastructure.Data.Repositories
             _ctx.Attach(cust).State = EntityState.Modified;
             _ctx.Entry(cust).Collection(c => c.Orders).IsModified = true;
             var orders = _ctx.Orders.Where(o => o.Customer.ID == cust.ID && !cust.Orders.Exists(co => co.ID == o.ID));
-            foreach (var order in orders) {
+            foreach (var order in orders)
+            {
                 order.Customer = null;
                 _ctx.Entry(order).Reference(o => o.Customer).IsModified = true;
             }
+            
             _ctx.SaveChanges();
             return cust;
         }
@@ -52,6 +54,8 @@ namespace ipcsmmd_webshop.Infrastructure.Data.Repositories
         public Customer Remove(int id)
         {
             Customer custRemoved = _ctx.Customers.Remove(new Customer { ID = id }).Entity;
+            IEnumerable<Order> ordersToRemove = _ctx.Orders.Where(o => o.Customer == custRemoved);
+            _ctx.RemoveRange(ordersToRemove);
             _ctx.SaveChanges();
             return custRemoved;
         }
